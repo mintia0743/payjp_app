439f045b267febfc2da566884d218349dfdea294
mintia0743 <160447778+mintia0743@users.noreply.github.com>
2024-04-20 15:42:47 +0900

    商品一覧の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_19_100611) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_20_063532) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
