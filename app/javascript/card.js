421bdfc788afc4ed39767d206f526646af631726
mintia0743 <160447778+mintia0743@users.noreply.github.com>
2024-04-25 19:44:48 +0900

    カード読み込み

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -9,12 +9,12 @@ const pay = () => {
   numberElement.mount('#number-form');
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
-};
-const form = document.getElementById("charge-form");
-form.addEventListener("submit", (e) => {
-  payjp.createToken(numberElement).then(function (response) {
-    if (response.error) {
-    } else {
+
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
         const token = response.id;
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         form.insertAdjacentHTML("beforeend", tokenObj);
@@ -22,9 +22,10 @@ form.addEventListener("submit", (e) => {
         expiryElement.clear();
         cvcElement.clear();
         document.getElementById("charge-form").submit();
-    }
-});
+      }
+    });
     e.preventDefault();
-});
+  });
+};
 
 window.addEventListener("turbo:load", pay);
